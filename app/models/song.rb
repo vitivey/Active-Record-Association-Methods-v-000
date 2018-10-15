65d6da1b55526abfa0c0a49c8a5c876da4d81206
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-15 17:22:24 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,9 +9,9 @@ class Song < ActiveRecord::Base
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
     if Artist.find_by(name: Drake)
-      drake = Artist.find_by(name: Drake) 
+      drake = Artist.find_by(name: Drake)
     else
-      drake = Artist.create(name: Drake) 
+      drake = Artist.create(name: Drake)
     end
     self.artist = drake
   end
